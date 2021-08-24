c1b80dd8173d1d6117871cb4a54a0120a04bf54f
liuhaojie <laotou99961@163.com>
2021-08-24 14:14:05 +0800

    oss

---
+++ gruul-oss/gruul-oss-open-service/src/main/java/com/medusa/gruul/oss/service/FileService.java
@@ -0,0 +1,20 @@
+
+
+package com.medusa.gruul.oss.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.medusa.gruul.oss.api.entity.FileEntity;
+import com.medusa.gruul.platform.api.model.dto.OssConfigDto;
+
+import java.util.List;
+
+/**
+ * 文件上传
+ */
+public interface FileService extends IService<FileEntity> {
+    OssConfigDto getConfig();
+
+    List<String> batchDownload(List<String> linkList);
+
+}
