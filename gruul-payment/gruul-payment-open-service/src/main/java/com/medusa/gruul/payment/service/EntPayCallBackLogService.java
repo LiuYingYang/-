7be728b1dbbd790fdbaa9e07f1f44038370f0c09
liuhaojie <laotou99961@163.com>
2021-09-01 16:05:05 +0800

    nacos

---
+++ gruul-payment/gruul-payment-open-service/src/main/java/com/medusa/gruul/payment/service/EntPayCallBackLogService.java
@@ -0,0 +1,14 @@
+package com.medusa.gruul.payment.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.medusa.gruul.common.core.util.Result;
+import com.medusa.gruul.payment.api.entity.EntPay;
+import com.medusa.gruul.payment.api.entity.EntPayCallBackLog;
+import com.medusa.gruul.payment.api.model.param.EntPayReQuestParam;
+
+/**
+ * @author create by zq
+ * @date created in 2019/11/18
+ */
+public interface EntPayCallBackLogService extends IService<EntPayCallBackLog> {
+}
