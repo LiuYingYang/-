139c9dd4c957d3e85601477b2a4ca0afe1eb47d7
Your Name <you@example.com>
2021-09-02 16:21:08 +0800

    x

---
+++ gruul-sms/gruul-sms-open-service/src/main/java/com/medusa/gruul/sms/SmsApplication.java
@@ -0,0 +1,24 @@
+package com.medusa.gruul.sms;
+
+import com.medusa.gruul.common.core.monitor.EnableMonitorHeartbeat;
+import com.medusa.gruul.common.swagger.annotation.EnableGruulSwagger2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import com.medusa.gruul.common.core.monitor.EnableMonitorHeartbeat;
+
+@EnableGruulSwagger2
+@SpringCloudApplication
+@EnableFeignClients(basePackages = "com.medusa.gruul.*.api.feign")
+@EnableDiscoveryClient
+@EnableScheduling
+
+public class SmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApplication.class, args);
+    }
+
+}
