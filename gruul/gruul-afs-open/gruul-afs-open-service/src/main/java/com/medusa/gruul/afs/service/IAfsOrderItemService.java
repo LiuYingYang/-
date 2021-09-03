e0c293aa5c8fba67cb49ba91466638d773905146
liuhaojie <laotou99961@163.com>
2021-09-03 16:35:13 +0800

    feat:售后模块

---
+++ gruul/gruul-afs-open/gruul-afs-open-service/src/main/java/com/medusa/gruul/afs/service/IAfsOrderItemService.java
@@ -0,0 +1,32 @@
+package com.medusa.gruul.afs.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.medusa.gruul.afs.api.entity.AfsOrderItem;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * <p>
+ * 申请的详情 服务类
+ * </p>
+ *
+ * @author alan
+ * @since 2020 -08-21
+ */
+public interface IAfsOrderItemService extends IService<AfsOrderItem> {
+
+    /**
+     * Gets exchange order.
+     *
+     * @param orderId the order id
+     * @return the exchange order
+     */
+    List<Long> getExchangeOrder(Long orderId);
+
+    /**
+     * 获取用户退款次数 -- 针对商品
+     * @param orderId
+     */
+    HashMap<String, Object> userApply(Long orderId);
+}
