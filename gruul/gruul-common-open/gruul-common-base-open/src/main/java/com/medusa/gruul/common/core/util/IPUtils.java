851ef73cc0b498f5ba369e84cc1ddbdd6572e21b
wangkang <miskwang@163.com>
2022-10-08 14:05:52 +0800

    ip多级反向代理非空判断

--- gruul/gruul-common-open/gruul-common-base-open/src/main/java/com/medusa/gruul/common/core/util/IPUtils.java
+++ gruul/gruul-common-open/gruul-common-base-open/src/main/java/com/medusa/gruul/common/core/util/IPUtils.java
@@ -43,7 +43,7 @@ public class IPUtils {
         }
 
         //使用代理，则获取第一个IP地址
-        if (StringUtils.isEmpty(ip) && ip.length() > 15) {
+        if (StringUtils.isNotEmpty(ip) && ip.length() > 15) {
             if (ip.indexOf(",") > 0) {
                 ip = ip.substring(0, ip.indexOf(","));
             }
